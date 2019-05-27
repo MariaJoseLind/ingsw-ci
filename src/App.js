8e8b97b927b47f730a855f0f69265f1eb4691604
Maria Lind <daniela.lind@globant.com>
2019-05-27 20:32:17 -0300

    add notification

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLIS, ESTOY ANDANDO
+          HOLIS MAJO, ESTOY ANDANDO Y MUESTRO NOTIFICACION
         </p>
         <a
           className="App-link"
