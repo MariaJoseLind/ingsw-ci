29fd87132b8fbaf684b164ecbd05cc2c8b020168
Maria Lind <daniela.lind@globant.com>
2019-06-02 17:48:35 -0300

    AGREGAR TITULO

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>TITULITO</h1>
         <p>
           HOLIS MAJO, ESTOY ANDANDO Y MUESTRO NOTIFICACION
         </p>
